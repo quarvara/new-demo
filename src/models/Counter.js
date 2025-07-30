edef7936d311666a64f9f170018d3ebc0980b92a
quarvara <pepemusk20@gmail.com>
2025-07-31 01:10:46 +0530

    Delete src directory

--- src/models/Counter.js
+++
@@ -1,8 +0,0 @@
-// models/Counter.js
-import mongoose from 'mongoose';
-
-const counterSchema = new mongoose.Schema({
-  count: { type: Number, default: 0 },
-});
-
-export default mongoose.models.Counter || mongoose.model('Counter', counterSchema);
--- src/pages/api/lib/cors.js
+++
@@ -1,9 +0,0 @@
-import Cors from 'cors';
-
-const cors = Cors({
-  methods: ['GET', 'POST', 'OPTIONS'],
-  origin: ['http://localhost:3000', 'https://new-demo-chi.vercel.app'],
-  credentials: true, // Optional: if you're using cookies or auth
-});
-
-export default cors;
